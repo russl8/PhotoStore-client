fdad0efd6bf925c5baf3e89705e1a38a44aad240
russl8 <russl8@my.yorku.ca>
2024-01-04 01:53:00 -0500

    eslintrc3

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
-    parser: '@typescript-eslint/parser',
-    extends: [
-      'plugin:react/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'prettier/@typescript-eslint',
-      'plugin:prettier/recommended',
-    ],
-    plugins: ['react', '@typescript-eslint', 'prettier'],
-    rules: {
-      // Your rules go here
-      'react-hooks/exhaustive-deps': 'off', // Disable the exhaustive-deps rule
-      '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
-    },
-  };
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'prettier/@typescript-eslint',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', '@typescript-eslint', 'prettier'],
+  rules: {
+    'react-hooks/exhaustive-deps': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { 'argsIgnorePattern': '^_' }],
+  },
+};
